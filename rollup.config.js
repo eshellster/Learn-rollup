b0687a2d9dbad749a290f4acda5aabd99461607e
eshell <eshellster@gmail.com>
2020-12-21 17:40:57 +0900

    외부 모듈도 포함시키는 플러그인 2가지

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from '@rollup/plugin-commonjs'
+
 export default {
     input: 'src/main.js',
     output: {
       dir: 'dist',
       format: 'cjs'
     },
-    external: [ 'faker' ]
+    external: [ 'faker' ],
+    plugins: [
+        nodeResolve(),
+        commonjs()
+      ]
   }
