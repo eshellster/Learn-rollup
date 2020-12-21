3201dfaa801e2d4f095d7a51a6495f58f110a44e
eshell <eshellster@gmail.com>
2020-12-21 19:59:34 +0900

    타입스크립트 사용

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,20 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import { terser } from "rollup-plugin-terser";
 
 export default {
-    input: 'src/main.js',
+    input: 'src/main.ts',
     output: {
       dir: 'dist',
-      format: 'cjs'
+      format: 'cjs',
+      exports:'named'
     },
     external: [ 'faker' ],
     plugins: [
         nodeResolve(),
-        commonjs()
+        commonjs({extensions:['.js','.ts']}),
+        typescript(),
+        terser()
       ]
   }
